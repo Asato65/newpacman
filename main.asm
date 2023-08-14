d3f457c4d53de863c3bfa381af8931ffabe90d6c
Asato65 <a.3ka319@gmail.com>
2023-08-14 10:13:52 +0900

    little fix

--- main.asm
+++ main.asm
@@ -17,50 +17,14 @@
 		ldx #0
 		stx ppu_update_data_pointer
 
-		lda #%11111100					; horizontal
+@STORE_PPU_DATA_LOOP:
+		lda PPU_DATA_ARR, x
+		cmp #$ff
+		beq @END_STORE
 		sta PPU_UPDATE_DATA, x
 		inx
-
-		lda #$20
-		sta PPU_UPDATE_DATA, x
-		inx
-		lda #$40
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #'A'
-		sta PPU_UPDATE_DATA, x
-		inx
-		lda #'B'
-		sta PPU_UPDATE_DATA, x
-		inx
-		lda #'C'
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #%11111110					; vertical
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #$22
-		sta PPU_UPDATE_DATA, x
-		inx
-		lda #$80
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #'1'
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #'2'
-		sta PPU_UPDATE_DATA, x
-		inx
-
-		lda #'3'
-		sta PPU_UPDATE_DATA, x
-		inx
-
+		bne @STORE_PPU_DATA_LOOP
+@END_STORE:
 		stx ppu_update_data_pointer
 
 		; endcode
@@ -77,3 +41,13 @@
 
 		jmp MAIN
 .endproc
+
+
+PPU_DATA_ARR:
+		.byte %11111100					; horizontal
+		.dbyt $2040						; dbyt = Define BYTe?: Define word sized data with the hi and lo bytes swapped. ($1234 = $12, $34)
+		.byte "ABC"
+		.byte %11111110					; vertical
+		.dbyt $2280
+		.byte "123"
+		.byte $ff						; loop end code
