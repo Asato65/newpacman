60e8741edfe94cbae7bece60090bd6469e29444d
Asato65 <a.3ka319@gmail.com>
2023-08-17 22:52:02 +0900

    add macro asr （複雑になりすぎたのでこの後削る）

--- asm/macro.asm
+++ asm/macro.asm
@@ -4,7 +4,6 @@
 ; @PARAM	ADDR: Array Address
 ; @PARAM	X Y: index (Access Arr[x][y])
 ; @BREAK	A tmp1
-; @RETURN	A
 ; ------------------------------------------------------------------------------
 
 .macro ldarr ADDR
@@ -57,8 +56,9 @@
 
 ; ------------------------------------------------------------------------------
 ; Addition
-; This Macro is not support Indirect addressing.
-; (Usage: add a, {$00, x})
+; This Macro only supports Immediate/Zeropage/Absolute addressing.
+; Other addressing modes are not optimized for speed.
+; Other addressing usage: add a, {$00, x} / add a, {($00), y}
 ; @PARAM	ARG1: register or Address
 ; @PARAM	VAL
 ; ------------------------------------------------------------------------------
@@ -75,9 +75,9 @@
 				adc VAL
 			.elseif (.match({ARG1}, x))
 				; add x, ??
-				.if (
-					.match(.left(1, {VAL}), #) &&
-					.right(.tcount({VAL})-1, {VAL}) <= 7
+				.if (\
+					.match(.left(1, {VAL}), #) &&\
+					.right(.tcount({VAL})-1, {VAL}) <= 7\
 				)
 					; add x, #0~7
 					.repeat (.right(.tcount({VAL})-1, {VAL}))
@@ -93,12 +93,12 @@
 				.endif
 			.elseif (.match({ARG1}, y))
 				; add y, ??
-				.if (
-					.match(.left(1, {VAL}), #) &&
-					.right(.tcount({VAL})-1, {VAL}) <= 7
+				.if (\
+					.match(.left(1, {VAL}), #) &&\
+					.right(.tcount({VAL})-1, {VAL}) <= 7\
 				)
 					; add y, #0~7
-					.repeat (.right (.tcount ({VAL})-1, {VAL}))
+					.repeat (.right(.tcount ({VAL})-1, {VAL}))
 						iny
 					.endrepeat
 				.else
@@ -111,12 +111,12 @@
 				.endif
 			.else
 				; add $00, ??
-				.if (
-					.match(.left(1, {VAL}), #) &&
-					.right(.tcount({VAL})-1, {VAL}) <= 2
+				.if (\
+					.match(.left(1, {VAL}), #) &&\
+					.right(.tcount({VAL})-1, {VAL}) <= 2\
 				)
 					; add $00, #0~2
-					.repeat (.right (.tcount ({VAL})-1, {VAL}))
+					.repeat (.right(.tcount ({VAL})-1, {VAL}))
 						inc ARG1
 					.endrepeat
 				.else
@@ -141,44 +141,158 @@
 ; ------------------------------------------------------------------------------
 
 .macro sub ARG1, VAL
-		sec
-	.if (.blank(ARG1) || .match({ARG1}, a))
-		sbc VAL
-	.elseif (.match({ARG1}, x))
-		pha
-		txa
-		sbc VAL
-		tax
-		pla
-	.elseif (.match({ARG1}, y))
-		pha
-		tya
-		sbc VAL
-		tay
-		pla
-	.else
-		pha
-		lda ARG1
-		sbc VAL
-		pla
+		.if (.paramcount = 1)
+			sec
+			sbc ARG1
+		.elseif (.paramcount = 2 && .match({ARG1}, a))
+			sec
+			sbc VAL
+		.elseif (.paramcount = 2 && .match({ARG1}, x))
+			.if (\
+				.match(.left(1, {VAL}), #) &&\
+				.right(.tcount({VAL})-1, {VAL}) <= 7\
+			)
+				.repeat (.right(.tcount({VAL})-1, {VAL}))
+					dex
+				.endrepeat
+			.else
+				pha
+				txa
+				sec
+				sbc VAL
+				tax
+				pla
+			.endif
+		.elseif (.paramcount = 2 && .match({ARG1}, y))
+			.if (\
+				.match(.left(1, {VAL}), #) &&\
+				.right(.tcount({VAL})-1, {VAL}) <= 7\
+			)
+				.repeat (.right(.tcount ({VAL})-1, {VAL}))
+					dey
+				.endrepeat
+			.else
+				pha
+				tya
+				sec
+				sbc VAL
+				tay
+				pla
+			.endif
+		.elseif (.paramcount = 2)
+			.if (\
+				.match(.left(1, {VAL}), #) &&\
+				.right(.tcount({VAL})-1, {VAL}) <= 2\
+			)
+				.repeat (.right(.tcount ({VAL})-1, {VAL}))
+					dec ARG1
+				.endrepeat
+			.else
+				pha
+				lda ARG1
+				sec
+				sbc VAL
+				sta ARG1
+				pla
+			.endif
+		.endif
 .endmacro
 
 
-
 ;*------------------------------------------------------------------------------
-; arithmetic right shift
-; @PARAM	ARG: register A or Address
+; Arithmetic right shift
+; ARG1 >>= C
+; @PARAM	ARG1: register A or Address
+; @PARAM	C: default=1
 ;*------------------------------------------------------------------------------
 
-.macro asr ARG
-	.if (.blank(ARG) || .match({ARG}, a))
-		cmp #%10000000					; Bit 7 into carry
-		ror								; Shift carry into bit 7
+.macro asr ARG1, C
+	.if (.paramcount = 1)
+		.ifblank(C)
+			; asr
+			cmp #%10000000					; Bit 7 into carry
+			ror								; Shift carry into bit 7
+		.elseif (.match(.left(1, {ARG1}), #))
+			; asr #4
+			.repeat (.right(.tcount ({ARG1})-1, {ARG1}))
+				cmp #%10000000					; Bit 7 into carry
+				ror								; Shift carry into bit 7
+			.endrepeat
+		.elseif (.addrsize(ARG1) <> 0)
+			; asr $80
+			stx tmp_rgstX
+			ldx ARG1
+			:
+			cmp #%10000000
+			ror
+			dex
+			bne :-
+			ldx tmp_rgstX
+		.endif
+	.elseif (.match({ARG1}, a))
+		.ifblank(C)
+			; asr a
+			cmp #%10000000					; Bit 7 into carry
+			ror								; Shift carry into bit 7
+		.elseif (.match(.left(1, {C}), #))
+			; asr a, #4
+			.repeat (.right(.tcount ({C})-1, {C}))
+				cmp #%10000000					; Bit 7 into carry
+				ror								; Shift carry into bit 7
+			.endrepeat
+		.elseif (.addrsize(C) <> 0)
+			; asr a, $40
+			stx tmp_rgstX
+			ldx C
+			:
+			cmp #%10000000
+			ror
+			dex
+			bne :-
+			ldx tmp_rgstX
+		.endif
 	.else
+		.ifblank(C)
+			; asr $80
+			pha
+			lda ARG1
+			cmp #%10000000
+			ror ARG1
+			pla
+		.elseif (.match(.left(1, {C}), #))
+			; asr $80, #??
+			pha
+			.if (.right(.tcount ({C})-1, {C}) = 1)
+				; asr $80, #1
+				lda ARG1
+				cmp #%1000_0000
+				ror ARG1
+			.else
+				lda ARG1
+				.repeat (.right(.tcount ({C})-1, {C}))
+					cmp #%10000000
+					ror
+				.endrepeat
+				sta ARG1
+			.endif
+			pla
+		.elseif (.addrsize(C) <> 0)
+			; asr $80, $40
+			stx tmp_rgstX
+			ldx C
+			:
+			cmp #%10000000
+			ror
+			dex
+			bne :-
+			ldx tmp_rgstX
+		.endif
 		pha
-		lda ARG
+		lda ARG1
 		cmp #%10000000
-		ror ARG
+		ror ARG1
 		pla
 	.endif
 .endmacro
+
+
--- pacman.asm
+++ pacman.asm
@@ -1,5 +1,7 @@
 .setcpu "6502"
-.feature c_comments
+.feature c_comments						; Allow C language type comments (/* comments */)
+.feature underline_in_numbers			; Allow notation %1010_0010
+.linecont +								; Allow line breaks in the middle of lines if you put a backslash at the end of the line
 
 .rodata									; ----- data -----
 .include "inc/const.inc"
