d2a2d7b7ac80cba2bd0d162eb7b6bd4287fff7f1
Asato65 <a.3ka319@gmail.com>
2023-10-26 08:11:17 +0900

    スプライトのサブルーチン作成

--- asm/sprite.asm
+++ asm/sprite.asm
@@ -1,28 +1,139 @@
 .proc _moveSprite
 	; ARG: A = sprite id
-	shl #2
+	shl #1
 	tax
-	lda SPR_BUFF, x
-	add move_dy
-	sta SPR_BUFF, x
-	lda SPR_BUFF+3, x
+
+	lda spr_buff, x
 	add move_dx
-	sta SPR_BUFF, x
+	sta spr_buff, x
+
+	lda spr_buff+1, x
+	add move_dy
+	sta spr_buff+1, x
+
 	rts
 	; ------------------------------
 .endproc
 
-.proc _tfrToSprBuff
-	; posy, sprid, attr, posx
+
+.proc _tfrToChrBuff
+	; posy,
+	sprid, attr, posx
 	; ARG: A = sprite id
-	shl #2
+	; ARG: Y = BUFF index
+	shl #1
 	tax
-	lda SPR_TO_CHAR, xuu
+
+	lda spr_buff, x						; pos X
+	sta tmp1
+	lda spr_buff+1, x					; pos Y
+	sta tmp2
+
+
+	tya
+	shl #2
+	tay
+	lda tmp2
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ID, x
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ATTR, X
+	sta CHR_BUFF, y
+
+	iny
+	lda tmp1
+	sta CHR_BUFF
 
 	inx
-	lda SPR_TO_CHAR, x
+
+	iny
+	lda tmp2
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ID, x
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ATTR, X
+	sta CHR_BUFF, y
+
+	iny
+	lda tmp1
+	add #8
+	sta CHR_BUFF
+
 	inx
-	lda SPR_TO_CHAR, x
+
+	iny
+	lda tmp2
+	add #8
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ID, x
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ATTR, X
+	sta CHR_BUFF, y
+
+	iny
+	lda tmp1
+	sta CHR_BUFF
+
 	inx
-	lda SPR_TO_CHAR, x
+
+	iny
+	lda tmp2
+	add #8
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ID, x
+	sta CHR_BUFF, y
+
+	iny
+	lda CHR_ATTR, X
+	sta CHR_BUFF, y
+
+	iny
+	lda tmp1
+	add #8
+	sta CHR_BUFF
+
+
+	/*
+	txa
+	shl #1
+	tax
+
+	lda CHR_ATTR, x
+
+	lda SPR_ID, x
+	sta CHR_BUFF, y
+	lda SPR_ATTR, x
+
+	inx
+	lda SPR_ID, x
+	add y, #4
+	sta CHR_BUFF, y
+
+	inx
+	lda SPR_ID, x
+	add y, #4
+	sta CHR_BUFF, y
+
+	inx
+	lda SPR_ID, x
+	add y, #4
+	sta CHR_BUFF, y
+	*/
+
+	rts
+	; ------------------------------
 .endproc
